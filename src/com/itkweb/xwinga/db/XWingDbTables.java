c987a61f193c5d7322940aa436ee7849e94f2c18
nangilella <nelly.angilella@itkweb.com>
2013-05-28 14:56:34 +0200

    remove upper case from image name

--- src/com/itkweb/xwinga/db/XWingDbTables.java
+++ src/com/itkweb/xwinga/db/XWingDbTables.java
@@ -26,25 +26,25 @@ public class XWingDbTables extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// on crée la table à partir de la requête écrite dans la variable
+		// on crï¿½e la table ï¿½ partir de la requï¿½te ï¿½crite dans la variable
 		// CREATE_BDD
 		db.execSQL(CREATE_BDD);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// On peut faire ce qu'on veut ici moi j'ai décidé de supprimer la table
-		// et de la recréer
-		// comme ça lorsque je change la version les id repartent de 0
+		// On peut faire ce qu'on veut ici moi j'ai dï¿½cidï¿½ de supprimer la table
+		// et de la recrï¿½er
+		// comme ï¿½a lorsque je change la version les id repartent de 0
 		db.execSQL("DROP TABLE " + TABLE_BOX + ";");
 		onCreate(db);
 	}
 
 	public void init() {
 		SQLiteDatabase db = getWritableDatabase();
-		// Création d'un ContentValues (fonctionne comme une HashMap)
+		// Crï¿½ation d'un ContentValues (fonctionne comme une HashMap)
 		ContentValues values = new ContentValues();
-		// on lui ajoute une valeur associé à une clé (qui est le nom de la
+		// on lui ajoute une valeur associï¿½ ï¿½ une clï¿½ (qui est le nom de la
 		// colonne dans laquelle on veut mettre la valeur)
 
 		for (BoxSet box : BoxSet.values()) {
@@ -52,22 +52,22 @@ public class XWingDbTables extends SQLiteOpenHelper {
 			values.put(COL_PICTURE, box.getPicture());
 		}
 
-		// on insère l'objet dans la BDD via le ContentValues
+		// on insï¿½re l'objet dans la BDD via le ContentValues
 		db.insert(TABLE_BOX, null, values);
 
 		db.close();
 	}
 
 	public enum BoxSet {
-	    b1 ("Boîte de base","SWX01.png"),
-	    b2 ("X-wing","SWX02.png"),
-	    b3 ("Chasseur TIE","SWX03.png"),
-	    b4 ("Y-wing","SWX04.png"),
-	    b5 ("TIE advanced","SWX05.png"),
-	    b6 ("Faucon Millenium","SWX06.png"),
-	    b7 ("Slave 1","SWX07.png"),
-	    b8 ("A-wing","SWX08.png"),
-	    b9 ("Intercepteur TIE","SWX09.png");
+	    b1 ("Boï¿½te de base","swx01.png"),
+	    b2 ("X-wing","swx02.png"),
+	    b3 ("Chasseur TIE","swx03.png"),
+	    b4 ("Y-wing","swx04.png"),
+	    b5 ("TIE advanced","swx05.png"),
+	    b6 ("Faucon Millenium","swx06.png"),
+	    b7 ("Slave 1","swx07.png"),
+	    b8 ("A-wing","swx08.png"),
+	    b9 ("Intercepteur TIE","swx09.png");
 
 	    private final String name;  
 	    private final String  picture; 
