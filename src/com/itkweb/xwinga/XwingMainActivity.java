7e901c377e9789129918ff0bd4098cdeaebb9f75
Cuong Lam <cuong.lam@itkweb.com>
2013-05-28 13:26:41 +0200

    Branchement boites

--- src/com/itkweb/xwinga/XwingMainActivity.java
+++ src/com/itkweb/xwinga/XwingMainActivity.java
@@ -1,5 +1,10 @@
 package com.itkweb.xwinga;
 
+import java.util.Set;
+
+import com.itkweb.xwinga.db.XWingDb;
+import com.itkweb.xwinga.model.Box;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -10,6 +15,12 @@ public class XwingMainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xwing_main_activity);
+        
+        //Création d'une instance de ma classe LivresBDD
+        XWingDb boxesBdd = new XWingDb(this);
+        Set<Box> boxes = boxesBdd.getAllBoxes();
+
+        
     }
 
 
