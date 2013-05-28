2065f4bb4cbb9de08653b9a9b7df2d9e991d4e94
nangilella <nelly.angilella@itkweb.com>
2013-05-28 14:28:23 +0200

    mv boxes in activity

--- src/com/itkweb/xwinga/XwingMainActivity.java
+++ src/com/itkweb/xwinga/XwingMainActivity.java
@@ -1,16 +1,11 @@
 package com.itkweb.xwinga;
 
 
-import java.util.Set;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-
-import com.itkweb.xwinga.db.XWingDb;
-import com.itkweb.xwinga.model.Box;
 
 public class XwingMainActivity extends Activity {
 
@@ -18,12 +13,6 @@ public class XwingMainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xwing_main_activity);
-        
-        //Cr�ation d'une instance de ma classe LivresBDD
-        XWingDb boxesBdd = new XWingDb(this);
-        Set<Box> boxes = boxesBdd.getAllBoxes();
-
-        
     }
 
 
@@ -36,14 +25,8 @@ public class XwingMainActivity extends Activity {
     
     public void start_repartition(View view) {
     	
-    	Intent intent = new Intent(this, XWingRepartitionActivityStep1.class);
-//    	EditText editText = (EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
+    	Intent intent = new Intent(this, XWingRepartitionStep1.class);
         startActivity(intent);
-
-    	
-        // Do something in response to button
     }
     
 }
